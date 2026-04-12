f38b68e861700c40293ff2675a9d78d65e6f9c35
utkarshkumar29 <ukyp2014@gmail.com>
2026-04-12 23:57:38 +0530

    bug fix

--- my-app/app/axios/axiosInstance.tsx
+++ my-app/app/axios/axiosInstance.tsx
@@ -1,32 +1,32 @@
 import axios from "axios"
 
-const baseURL= "https://devforum-gateway.onrender.com/api"
+const baseURL = "https://devforum-gateway.onrender.com/api"
 
-export const axiosPublic=axios.create({
-    baseURL:baseURL,
-    withCredentials:true,
-    headers:{
+export const axiosPublic = axios.create({
+    baseURL: baseURL,
+    withCredentials: true,
+    headers: {
         "Content-Type": "application/json"
     }
 })
 
-export const axiosPrivate=axios.create({
-    baseURL:baseURL,
-    withCredentials:true,
-    headers:{
+export const axiosPrivate = axios.create({
+    baseURL: baseURL,
+    withCredentials: true,
+    headers: {
         "Content-Type": "application/json"
     }
 })
 
 axiosPrivate.interceptors.request.use(
-    (config)=>{
-        const token=localStorage.getItem("accessToken")
-        console.log(token,"please")
-        if(token){
-            config.headers.Authorization=`Bearer ${token}`
+    (config) => {
+        const token = localStorage.getItem("accessToken")
+        console.log(token, "please")
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
         }
         return config
     },
-    (error)=>Promise.reject(error)
+    (error) => Promise.reject(error)
 )
 
--- my-app/app/components/feed/FeedComments.tsx
+++ my-app/app/components/feed/FeedComments.tsx
@@ -4,53 +4,53 @@ import Image from "next/image"
 import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 
-interface UserInfo{
-    _id:string,
-    display_name:string,
-    photo_url:string
+interface UserInfo {
+    _id: string,
+    display_name: string,
+    photo_url: string
 }
 
 interface Comment {
-  _id: string;
-  text: string;
-  user: UserInfo;
-  createdAt: string;
+    _id: string;
+    text: string;
+    user: UserInfo;
+    createdAt: string;
 }
 
 interface PostType {
-  slug: string;
-  user: UserInfo;
+    slug: string;
+    user: UserInfo;
 }
 
-const FeedComments=({ post }:{ post: PostType })=>{
-    const [commentText,setCommentText]=useState("")
-    const [commentPage,setCommentPage]=useState(1)
-    const [userComments,setUserComments]=useState<Comment[]>([])
-    const [totalPages,setTotalPages]=useState(1)
-    const [nextCommentUrl,setNextcommentUrl]=useState(null)
-    
-    const handleAddComment=async()=>{
+const FeedComments = ({ post }: { post: PostType }) => {
+    const [commentText, setCommentText] = useState("")
+    const [commentPage, setCommentPage] = useState(1)
+    const [userComments, setUserComments] = useState<Comment[]>([])
+    const [totalPages, setTotalPages] = useState(1)
+    const [nextCommentUrl, setNextcommentUrl] = useState(null)
+
+    const handleAddComment = async () => {
         try {
-            const response=await axiosPrivate.post(`/posts/addComment/${post?.slug}`,{commentText})
+            const response = await axiosPrivate.post(`/posts/addComment/${post?.slug}`, { commentText })
             setCommentText("")
-            setUserComments((prev)=>[response.data.comment,...prev])
+            setUserComments((prev) => [response.data.comment, ...prev])
         } catch (error) {
             console.log(error)
         }
     }
 
-    const fetchComments=async()=>{
+    const fetchComments = async () => {
         try {
-            const response=await axiosPrivate.get(nextCommentUrl ?? `/posts/getComments/${post?.slug}?page=${commentPage}`)
-            console.log(response,"plawse")
-            if(userComments.length>0){
-                setUserComments((prev)=>[...prev,...response.data.comments])
-            }else{
+            const response = await axiosPrivate.get(nextCommentUrl ?? `/posts/getComments/${post?.slug}?page=${commentPage}`)
+            console.log(response, "plawse")
+            if (userComments.length > 0) {
+                setUserComments((prev) => [...prev, ...response.data.comments])
+            } else {
                 setUserComments(response.data.comments)
             }
             setCommentPage(response.data.page)
             setTotalPages(response.data.totalPages)
-            if(response.data.nextUrl){
+            if (response.data.nextUrl) {
                 setNextcommentUrl(response.data.nextUrl)
             }
         } catch (error) {
@@ -58,25 +58,25 @@ const FeedComments=({ post }:{ post: PostType })=>{
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchComments()
-    },[commentPage])
+    }, [commentPage])
 
-    return(
+    return (
         <div className=" flex flex-col gap-4 border border-[#262626] p-4 rounded-[8px] mt-4 h-auto  ">
             <div className=" flex gap-2 ">
                 <Image src={post?.user?.photo_url} alt="User" width={20} height={20} className=" max-h-[20px] rounded-full border border-purple-600 " />
-            <div className=" flex w-full flex-col ">
-                <textarea onChange={(e)=>setCommentText(e.target.value)} className=" w-full outline-none bg-[#181621] border border-[#262626] p-[24px] rounded-[8px] h-[100px] " placeholder="Add a comment...." />
-                <div className=" flex justify-end mt-3 ">
-                    <button className=" bg-[#7D42F5] px-4 py-2 rounded-[8px] " onClick={handleAddComment}>Post Comment</button>
+                <div className=" flex w-full flex-col ">
+                    <textarea onChange={(e) => setCommentText(e.target.value)} className=" w-full outline-none bg-[#181621] border border-[#262626] p-[24px] rounded-[8px] h-[100px] " placeholder="Add a comment...." />
+                    <div className=" flex justify-end mt-3 ">
+                        <button className=" bg-[#7D42F5] px-4 py-2 rounded-[8px] " onClick={handleAddComment}>Post Comment</button>
+                    </div>
                 </div>
-            </div>
             </div>
 
             <div className=" flex flex-col gap-2 ">
-                {userComments && userComments.length>0 && userComments.map((comment,index)=>{
-                    return(
+                {userComments && userComments.length > 0 && userComments.map((comment, index) => {
+                    return (
                         <div className=" flex gap-2 " key={index}>
                             <Image src={comment?.user?.photo_url} alt="User" width={20} height={20} className=" max-h-[20px] rounded-full border border-purple-600 " />
                             <div className=" bg-[#252235] w-full h-full px-[24px] py-[16px] rounded-[8px] ">
@@ -87,7 +87,7 @@ const FeedComments=({ post }:{ post: PostType })=>{
                     )
                 })}
 
-                {commentPage<totalPages && (
+                {commentPage < totalPages && (
                     <div className=" flex w-full flex justify-center items-center mt-4 ">
                         <button onClick={fetchComments} className=" text-[#7D42F5] cursor-pointer ">Load More Comments....</button>
                     </div>
